b992e8629d0a9ac3899ddee3627d4e56c02a7c37
stargazing4122 <jadenyeiden@gmail.com>
2022-01-05 18:48:52 -0500

    🔢 logic statistics component added

--- src/todo/components/StatisticsTodo.tsx
+++ src/todo/components/StatisticsTodo.tsx
@@ -1,19 +1,22 @@
 import React from 'react';
+import useTodosContext from '../hooks/useTodosContext';
 
 const StatisticsTodo = () => {
+  const { todosState } = useTodosContext();
+
   return (
     <div className="row">
       <div className="col-4 text-center">
         <h2>Total todos</h2>
-        <p className="fs-1">2</p>
+        <p className="fs-1">{todosState.completed}</p>
       </div>
       <div className="col-4 text-center">
         <h2>Completed</h2>
-        <p className="fs-1">1</p>
+        <p className="fs-1">{todosState.completed}</p>
       </div>
       <div className="col-4 text-center">
         <h2>Pendings</h2>
-        <p className="fs-1">1</p>
+        <p className="fs-1">{todosState.pending}</p>
       </div>
     </div>
   );
