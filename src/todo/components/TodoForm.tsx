b88ebbbaf851f68f6894aef730f0c5a3c5f38d2a
stargazing4122 <jadenyeiden@gmail.com>
2022-01-05 19:27:50 -0500

    🖊️ logic of add todo

--- src/todo/components/TodoForm.tsx
+++ src/todo/components/TodoForm.tsx
@@ -1,18 +1,47 @@
-import React from 'react';
+import React, { FormEvent } from 'react';
+import useForm from '../hooks/useForm';
+import useTodosContext from '../hooks/useTodosContext';
+import { Todo } from '../interfaces/interfaces';
 
+interface FormData {
+  description: string;
+}
 const TodoForm = () => {
+  const { addTodo } = useTodosContext();
+
+  const { formValues, handleChange, formReset } = useForm<FormData>({
+    description: '',
+  });
+
+  const { description } = formValues;
+
+  const handleAddTodo = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (description.trim().length > 2) {
+      const newTodo: Todo = {
+        id: new Date().getTime().toString(),
+        desc: description,
+        completed: false,
+      };
+      addTodo(newTodo);
+      formReset();
+    }
+  };
+
   return (
     <div>
       <h2>Add a Todo</h2>
-      <form>
+      <form onSubmit={handleAddTodo}>
         <input
           type="text"
           className="form-control mb-3"
           placeholder="Input a todo"
           autoComplete="off"
-          name="todoName"
+          name="description"
+          value={description}
+          onChange={handleChange}
         />
-        <button className="btn btn-outline-success" type="button">
+        <button className="btn btn-outline-success" type="submit">
           Add
         </button>
       </form>
